52c1eeb2ecb229dc1486043d8ddfbe7cb9200261
github-suraj <surajkumarjaiswal053@gmail.com>
2022-06-01 22:54:23 +0530

    DRF: Create news apis with APIView class

--- newsapi/news/api/urls.py
+++ newsapi/news/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from news.api.views import article_list, article_detail
+# from news.api.views import article_list, article_detail
+from news.api.views import ArticleList, ArticleDetail
 
 urlpatterns = [
-    path('articles/', article_list, name='article-list'),
-    path('articles/<int:pk>/', article_detail, name='article-detail'),
+    # path('articles/', article_list, name='article-list'),
+    # path('articles/<int:pk>/', article_detail, name='article-detail'),
+    path('articles/', ArticleList.as_view(), name='article-list'),
+    path('articles/<int:pk>/', ArticleDetail.as_view(), name='article-detail'),
 ]
